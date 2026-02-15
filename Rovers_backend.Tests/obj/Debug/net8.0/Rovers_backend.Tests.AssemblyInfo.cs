64b54b3a3bd73a3fd31421972a35090f115357dd
vinodabhik-proj <vinodabhik@gmail.com>
2026-02-15 15:31:49 +0000

    Adding dependencies for in memory testing

--- Rovers_backend.Tests/obj/Debug/net8.0/Rovers_backend.Tests.AssemblyInfo.cs
+++ Rovers_backend.Tests/obj/Debug/net8.0/Rovers_backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rovers_backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85ce1b4e9a0c2e04daddcee5d09d4237ae7112e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1dc11ad4a701eccc3c23bbb88fd5f474690b699")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rovers_backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rovers_backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
